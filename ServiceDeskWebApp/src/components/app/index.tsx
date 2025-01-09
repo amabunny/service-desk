a33dc463a73fae38428a4fa5b84209944edf63c3
Sergey <iamgreyy@gmail.com>
2025-01-10 02:11:59 +0500

    feat(*): remove antd and install tailwind

--- ServiceDeskWebApp/src/components/app/index.tsx
+++ ServiceDeskWebApp/src/components/app/index.tsx
@@ -1,13 +1,5 @@
-import { ConfigProvider } from 'antd';
-
 import { Router } from '../router';
 
 export const App = () => {
-  return (
-    <ConfigProvider
-      theme={{ cssVar: true, components: { Layout: { headerHeight: 44 } } }}
-    >
-      <Router />
-    </ConfigProvider>
-  );
+  return <Router />;
 };
